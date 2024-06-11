c93058653abd602e44a5afc2c93cada955bc3f9f
Beatriz Monteiro <biafigmonteiro@gmail.com>
2024-06-11 16:28:39 -0300

    modified index and design

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import ContactList from '../components/ContactList.vue';
-import ContactDetail from '../components/ContactDetail.vue';
-import AddContact from '../components/AddContact.vue';
+import { createRouter, createWebHistory } from 'vue-router'; 
+//importando duas funções do pacote vue router
+//createRouter cria uma instancia de roteador
+//createWebHistory cria uma instancia de historico de navegacao baseada em HTML5
 
-const routes = [
-  { path: '/', component: ContactList },
+import ContactList from '../components/ContactList.vue';
+//importando o componente ContactList.vue
+
+import ContactDetail from '../components/ContactDetail.vue'
+//importando o componente ContactDetail.vue 
+
+import AddContact from '../components/AddContact.vue';
+//importando o componente AddContact
+
+
+/*Esses componentes são páginas individuais da aplicação,
+ que serão renderizadas quando as rotas correspondentes forem acessadas.
+*/
+
+
+const routes = [ //array de objetos, com as rotas da aplicação
+
+  //path define o caminho da url referente ao component
+  { path: '/', component: ContactList }, 
   { path: '/contact/:id', component: ContactDetail },
   { path: '/add', component: AddContact }
 ];
 
+//criando uma instancia (variavel) com um objeto
+//utiliza a funcao createRoute 
+
+//objeto de configuracao para a funcao, com history
+//chamando a funcao Web para armazenar o historico
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes, //lista de rotas definidas antes
 });
 
-export default router;
+export default router; //exportando a instancia do roteador
+//assim, ela poderá ser importada em outras partes do
+//projeto
