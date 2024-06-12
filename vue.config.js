78e4b8277d91db90708717ca2d490c404b1d0a07
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-06-12 16:57:43 -0300

    add mvc structure and global register (#4)

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: process.env.BASE_URL,
+        changeOrigin: true,
+        pathRewrite: { "^/api": "" },
+      },
+    },
+  },
+};
