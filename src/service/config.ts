7fceb00a436efd55884145067f0b4d9ff52af9e6
Beatriz Monteiro <biafigmonteiro@gmail.com>
2024-06-17 16:58:17 -0300

    added rxjs

--- src/service/config.ts
+++ src/service/config.ts
@@ -1,18 +1,51 @@
-import axios from "axios";
-const api = axios.create({baseURL:"/api"})
+import axios, {AxiosError, AxiosInstance, AxiosRequestConfig} from "axios";
+//const api = axios.create({baseURL:"/api"})
 
-api.interceptors.request.use(
-    (request) => {
-        return request;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
+function apiConfig(baseUrl: string): AxiosRequestConfig{
+    return {
+        baseURL: baseUrl,
+    };
+}
 
-api.interceptors.response.use(
-    (response) => response, (error) => {alert(error);
-    return Promise.reject(error);}
-);
+function initAxios(config: AxiosRequestConfig, token?: any): AxiosInstance{
+    const defineInstance = axios.create(config);
+    defineInstance.interceptors.request.use(
+        (request: AxiosRequestConfig) => {
+            return request;
+        },
+        (error) => Promise.reject(error)
+    );
+
+    defineInstance.interceptors.response.use (
+        (response) => response,
+        (error: AxiosError) => {
+            alert(error);
+            return Promise.reject(error);
+        }
+    );
+
+    return defineInstance;
+
+}
+
+function api(baseURL="/api", token?: any){
+    return initAxios(apiConfig(baseURL), token);
+}
 
 export default api;
+
+// api.interceptors.request.use(
+//     (request) => {
+//         return request;
+//     },
+//     (error) => {
+//         return Promise.reject(error);
+//     }
+// );
+
+// api.interceptors.response.use(
+//     (response) => response, (error) => {alert(error);
+//     return Promise.reject(error);}
+// );
+
+// export default api;
--- src/service/rest/contact.rest.ts
+++ src/service/rest/contact.rest.ts
@@ -1,5 +1,5 @@
 
-import api from '../config'
+import api from '../rxjs'
 import { Contact} from '@/model/contact.model';
 
 export class ContactRest {
